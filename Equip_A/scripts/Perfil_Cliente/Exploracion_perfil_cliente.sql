7dcb7b4b024b8e867c8f22c3def75c803fdddb2f
DamianMolini <damianmolini@gmail.com>
2024-10-10 10:41:43 +0200

    Exploración, creación de vista y consultas

--- Equip_A/scripts/Perfil_Cliente/Exploracion_perfil_cliente.sql
+++ Equip_A/scripts/Perfil_Cliente/Exploracion_perfil_cliente.sql
@@ -1,181 +1,192 @@
-USE EquipA;
-
+# Columnas, tipos de datos y valores nulos
 DESCRIBE EquipA.BANK_marketing;
 
+# Primer pantallazo del dataset
 SELECT * FROM EquipA.BANK_marketing;
 
-WITH duplicate_CTE AS 
-(
-	SELECT *,
-		ROW_NUMBER() OVER(PARTITION BY age, job, marital, education, `default`, 
-        balance, housing, loan, contact, `day`, `month`, duration, campaign, pdays, previous, poutcome, deposit) AS row_n
-FROM EquipA.BANK_marketing
-)
-
-SELECT * 
-FROM duplicate_CTE
-WHERE row_n > 1;
-
-SELECT COUNT(*)
+# Número de registros: 11162
+SELECT COUNT(*) AS observations
 FROM EquipA.BANK_marketing;
 
-SELECT * 
-FROM EquipA.BANK_marketing
-WHERE age IS NULL;
-
+# Valores únicos y número de registros por trabajo
 SELECT job, COUNT(*) AS observations
 FROM EquipA.BANK_marketing
 GROUP BY job
 ORDER BY observations DESC;
 
-WITH CTE AS (
-
-	SELECT job,
-		SUM(CASE WHEN deposit = "yes" THEN 1 ELSE 0 END) AS deposits,
-		SUM(CASE WHEN deposit = "no" THEN 1 ELSE 0 END) AS rejected
-	FROM EquipA.BANK_marketing
-	GROUP BY job
-	ORDER BY deposits DESC
-)
-
-SELECT job, 
-	deposits,
-    rejected,
-	ROUND(deposits / (deposits + rejected) * 100, 2) AS deposit_rate
-FROM CTE
-ORDER BY deposit_rate DESC;
-
-SELECT marital, COUNT(*) 
+# Valores únicos y número de registros por estado civil
+SELECT marital, COUNT(*) AS observations
 FROM EquipA.BANK_marketing
 GROUP BY marital
 ORDER BY COUNT(*) DESC;
 
-SELECT *
-FROM EquipA.BANK_marketing
-WHERE marital IS NULL;
-
-SELECT education, COUNT(*) 
+# Valores únicos y número de registros por tipo de educación
+SELECT education, COUNT(*) AS observations
 FROM EquipA.BANK_marketing
 GROUP BY education
 ORDER BY COUNT(*) DESC;
 
-SELECT *
-FROM EquipA.BANK_marketing
-WHERE education IS NULL;
-
-SELECT education, COUNT(*)
-FROM EquipA.BANK_marketing
-WHERE job = "management"
-GROUP BY education;
-
-SELECT `default`, COUNT(*) 
+# Valores únicos y número de registros por default
+SELECT `default`, COUNT(*) AS observations
 FROM EquipA.BANK_marketing
 GROUP BY `default`
 ORDER BY COUNT(*) DESC;
 
-SELECT housing, COUNT(*) 
+# Valores únicos y número de registros por hipoteca
+SELECT housing, COUNT(*) AS observations
 FROM EquipA.BANK_marketing
 GROUP BY housing
 ORDER BY COUNT(*) DESC;
 
-SELECT loan, COUNT(*) 
+# Valores únicos y número de registros por préstamo
+SELECT loan, COUNT(*) AS observations
 FROM EquipA.BANK_marketing
 GROUP BY loan
 ORDER BY COUNT(*) DESC;
 
-SELECT contact, COUNT(*) 
+# Valores únicos y número de registros por tipo de contacto
+SELECT contact, COUNT(*) AS observations
 FROM EquipA.BANK_marketing
 GROUP BY contact
 ORDER BY COUNT(*) DESC;
 
-SELECT campaign, COUNT(*) 
+# Valores únicos y número de registros por campaña
+SELECT campaign, COUNT(*) AS observations
 FROM EquipA.BANK_marketing
 GROUP BY campaign
 ORDER BY COUNT(*) DESC;
 
-SELECT previous, COUNT(*) 
+# Valores únicos y número de registros por contactos previos
+SELECT previous, COUNT(*) AS observations
 FROM EquipA.BANK_marketing
 GROUP BY previous
 ORDER BY COUNT(*) DESC;
 
-SELECT poutcome, COUNT(*) 
+# Valores únicos y número de registros por previous outcome
+SELECT poutcome, COUNT(*) AS observations
 FROM EquipA.BANK_marketing
 GROUP BY poutcome
 ORDER BY COUNT(*) DESC;
 
-SELECT DISTINCT previous, poutcome, COUNT(*)
-FROM EquipA.BANK_marketing
-WHERE poutcome = "Unknown"
-GROUP BY previous, poutcome;
-
-SELECT deposit, COUNT(*) 
+# Valores únicos y número de registros por resultado de la llamada
+SELECT deposit, COUNT(*) AS observations
 FROM EquipA.BANK_marketing
 GROUP BY deposit
 ORDER BY COUNT(*) DESC;
 
+# Creamos vista para perfil de cliente. Nos quedamos solo con las variables demográficas y agregamos dos nuevas columnas: age_slot y balance_binary
 CREATE VIEW perfil_Cliente AS (
-	SELECT *
-    FROM EquipA.BANK_marketing
-);
-
-SELECT *
-FROM EquipA.BANK_marketing;
-
-SELECT COUNT(*)
-FROM EquipA.BANK_marketing;
-
-ALTER VIEW perfil_Cliente AS (
 	SELECT id, 
+		age,
 			CASE 
-				WHEN age < 25 THEN "under 25"
-				WHEN age >= 25 AND age < 30 THEN "25-29"
-                WHEN age >= 30 AND age < 35 THEN "30-34"
-                WHEN age >= 35 AND age < 40 THEN "35-39"
-                WHEN age >= 40 AND age < 45 THEN "40-44"
-                WHEN age >= 45 AND age < 50 THEN "45-49"
-                WHEN age >= 50 AND age < 55 THEN "50-54"
-                WHEN age >= 55 AND age < 60 THEN "55-59"
-                ELSE "60+"
-                END AS age_group,
-			job,
-            marital,
-            education,
+				WHEN age < 20 THEN "1X"
+				WHEN age >= 20 AND age < 30 THEN "2X"
+                WHEN age >= 30 AND age < 40 THEN "3X"
+                WHEN age >= 40 AND age < 50 THEN "4X"
+                WHEN age >= 50 AND age < 60 THEN "5X"
+                WHEN age >= 60 AND age < 70 THEN "6X"
+                WHEN age >= 70 AND age < 80 THEN "7X"
+                WHEN age >= 80 AND age < 90 THEN "8X"
+                ELSE "9X"
+                END AS age_slot,
+			job, marital, education,
+			balance,
             CASE 
-				WHEN education = "tertiary" THEN "yes"
-                ELSE "no" 
-                END AS college_university,
-			`default`,
-            balance,
-            housing,
-            loan,
-            contact,
-            `day`,
-            `month`,
-            duration,
-            pdays,
-            previous,
-            poutcome,
-            deposit
+				WHEN balance >= 0 THEN "positive" ELSE "negative" END AS balance_binary,
+			deposit
     FROM EquipA.BANK_marketing
 );
 
 SELECT * FROM EquipA.perfil_Cliente;
 
+SELECT age_slot, count(*) FROM EquipA.perfil_Cliente group by age_slot;
 
-WITH CTE AS 
+SELECT COUNT(*)
+FROM EquipA.BANK_marketing;
+
+# Analizamos el porcentaje de depósitos realizados para cada grupo de edad
+WITH cte AS 
 (
-	SELECT age_group,
+	SELECT age_slot,
     SUM(CASE WHEN deposit = "yes" THEN 1 ELSE 0 END) AS deposits,
     COUNT(*) AS total_calls
     FROM EquipA.perfil_Cliente
-	GROUP BY age_group
+	GROUP BY age_slot
 )
-SELECT age_group, 
+SELECT age_slot, 
 	deposits,
     total_calls,
 	ROUND(deposits / total_calls * 100, 2) AS deposit_rate
-FROM CTE
+FROM cte
 ORDER BY 4 DESC;
 
+# Sumamos el tipo de trabajo
+WITH cte AS 
+(
+	SELECT age_slot,
+    job,
+    SUM(CASE WHEN deposit = "yes" THEN 1 ELSE 0 END) AS deposits,
+    COUNT(*) AS total_calls
+    FROM EquipA.perfil_Cliente
+	GROUP BY age_slot, job
+)
+SELECT age_slot, 
+		job
+	deposits,
+    total_calls,
+	ROUND(deposits / total_calls * 100, 2) AS deposit_rate
+FROM cte
+WHERE age_slot NOT IN ("3X", "4X", "5X") 
+ORDER BY age_slot, deposit_rate DESC;
 
+# Depósitos de las personas con balance anual negativo
+SELECT 
+	SUM(CASE WHEN deposit = "yes" THEN 1 ELSE 0 END) AS deposits,
+	COUNT(*) AS total_calls,
+    ROUND(SUM(CASE WHEN deposit = "yes" THEN 1 ELSE 0 END) / COUNT(*) * 100, 2) AS deposit_rate
+FROM EquipA.perfil_Cliente
+WHERE balance_binary = "negative";
 
+WITH cte AS 
+(
+	SELECT job, 
+			education,
+			marital,
+			SUM(CASE WHEN deposit = "yes" THEN 1 ELSE 0 END) AS deposits,
+			COUNT(*) AS total_calls
+	FROM EquipA.perfil_Cliente
+    WHERE job <> "unknown"
+    AND education <> "unknown"
+	GROUP BY job, education, marital
+)
+SELECT job, 
+		education,
+		marital,
+        deposits,
+		total_calls,
+		ROUND(deposits / total_calls * 100, 2) AS deposit_rate
+FROM cte
+ORDER BY 6 DESC;
+
+WITH cte AS 
+(
+	SELECT job, 
+			SUM(CASE WHEN deposit = "yes" THEN 1 ELSE 0 END) AS deposits,
+			COUNT(*) AS total_calls
+	FROM EquipA.perfil_Cliente
+    WHERE job <> "unknown"
+	GROUP BY job
+)
+SELECT job, 
+        deposits,
+		total_calls,
+		ROUND(deposits / total_calls * 100, 2) AS deposit_rate
+FROM cte
+ORDER BY deposit_rate DESC;
+
+SELECT *
+FROM EquipA.perfil_Cliente
+WHERE job = "student";
+
+SELECT AVG(age)
+FROM EquipA.perfil_Cliente;
