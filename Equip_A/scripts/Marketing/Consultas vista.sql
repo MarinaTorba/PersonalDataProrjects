012e5fe4f6327e23c705e5d034b3936cedb97e30
laura0217 <119606511+laura0217@users.noreply.github.com>
2024-10-11 18:34:55 +0200

    Actualización ficheros sql: creación de vista y consultas vista

--- Equip_A/scripts/Marketing/Consultas vista.sql
+++ Equip_A/scripts/Marketing/Consultas vista.sql
@@ -46,3 +46,32 @@ SELECT campaign,
 FROM v_marketing
 GROUP BY campaign
 ORDER BY campaign ASC;
+
+/* Consulta duration_slot por deposit */
+SELECT duration_slot,deposit,COUNT(id)  
+FROM v_marketing 
+GROUP BY duration_slot,deposit 
+ORDER BY duration_slot;
+
+/* Consulta duration_slot por deposit Y contact */
+SELECT contact,duration_slot, deposit, COUNT(id) 
+FROM v_marketing 
+GROUP BY duration_slot,contact, deposit 
+ORDER BY contact, duration_slot,deposit;
+
+/* Consulta age_slot por duration_slot */
+SELECT age_slot,duration_slot,COUNT(id)  
+FROM v_marketing 
+GROUP BY age_slot,duration_slot 
+ORDER BY COUNT(id) desc;
+
+/* Consulta  distintos valores de pdays_slot */
+SELECT DISTINCT pdays_slot,COUNT(id) 
+FROM EquipA.v_marketing 
+GROUP BY pdays_slot 
+ORDER BY pdays_slot;
+
+/* Consulta más de 20 llamadas recibidas */
+SELECT COUNT(id) 
+FROM EquipA.v_marketing 
+WHERE campaign>20;
--- Equip_A/scripts/Marketing/creacion vista.sql
+++ Equip_A/scripts/Marketing/creacion vista.sql
@@ -1,8 +1,33 @@
 USE EquipA;
+/* 
+Creación Vista v_marketing 
+Se modifica el nombre de day y month para mayor comodidad en lAS queries y explicación de lo que representa el campo
+Se añaden los campos calculados duration_slot, age_slot y pdays_slot para categorizar los datos en franjAS
+*/
 
-Create  view v_marketing as 
-(select id, age,job, marital, education,`default` as default_credit, contact, `day` as 'contact_day', `month` as 'contact_month',
-duration,campaign, pdays, previous,poutcome, deposit, IF (duration<150 ,'d02',IF(duration < 300,'d05', IF (duration <450,'d07', 
-If (duration <600,'d10','dmas10')))) as duration_slot, concat(substring(age,1,1) ,'X') as age_slot
- from EquipA.BANK_marketing
+CREATE  view v_marketing AS 
+(SELECT id, age,job, marital, education,`default` AS default_credit, contact, `day` AS 'contact_day', `month` AS 'contact_month',
+duration, campaign, pdays, previous, poutcome, deposit, 
+CONCAT(substring(age,1,1) ,'X') AS age_slot, 
+	IF (duration<150 ,'d02',IF(duration < 300,'d05', 
+    IF (duration <450,'d07', 
+	If (duration <600,'d10',
+    'dmAS10')))) 
+AS duration_slot, 
+	IF (pdays=-1 ,'nc',IF(pdays < 30,'m01', 
+	IF (pdays <60,'m02', 
+	If (pdays <90,'m03',
+	IF(pdays < 120,'m04',
+	IF(pdays < 150,'m05',
+	IF(pdays < 180,'m06',
+	IF(pdays < 210,'m07',
+	IF(pdays < 240,'m08',
+	IF(pdays < 270,'m09',
+	IF(pdays < 300,'m10',
+	IF(pdays < 330,'m11',
+	IF(pdays < 360,'m12',
+	IF(pdays < 450,'m15',
+	'mAS15'))))))))))))))   
+AS pdays_slot 
+FROM EquipA.BANK_marketing
 );
